871295b7fc3d61776a567bdcb4e74fe968c58a50
Ivanilton Bezerra <nitinhosilva1996@gmail.com>
2020-10-04 21:42:12 -0300

    Custom Hook - criando useLocalStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import Produto from "./Produto";
-import { GlobalStorage } from "./GlobalContext";
+import useLocalStore from "./useLocalStorage";
 
 const App = () => {
+  const [product, setProduct] = useLocalStore("product", "");
+
+  const handleClick = ({ target }) => setProduct(target.innerText);
+
   return (
-    <GlobalStorage>
-      <Produto />
-    </GlobalStorage>
+    <>
+      <p>Produto preferido: {product}</p>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </>
   );
 };
 
