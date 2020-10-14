70b7c66586a51885dac3df1c23e7b504626d04e0
Ivanilton Bezerra <nitinhosilva1996@gmail.com>
2020-10-14 12:03:24 -0300

    Componente Checkbox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Checkbox from "./Form/Checkbox";
 import Input from "./Form/Input";
 import Radio from "./Form/Radio";
 import Select from "./Form/Select";
@@ -8,15 +9,28 @@ const App = () => {
   const [email, setEmail] = useState("");
   const [product, setProduct] = useState("");
   const [cor, setCor] = useState("");
+  const [languages, setLanguages] = useState([]);
 
   return (
     <form>
+      <h2>Checkbox</h2>
+      <Checkbox
+        options={["JS", "PHP"]}
+        value={languages}
+        setValue={setLanguages}
+      />
+
+      <h2>Radio</h2>
       <Radio options={["Azul", "Red"]} value={cor} setValue={setCor} />
+
+      <h2>Select</h2>
       <Select
         options={["teste", "teste1"]}
         value={product}
         setValue={setProduct}
       />
+
+      <h2>Input</h2>
       <Input id="name" label="Nome" value={name} setValue={setName} />
       <Input
         id="email"
